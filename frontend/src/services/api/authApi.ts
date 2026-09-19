c2db04e4940500d588a1d537043ce50209bfd7f8
Friliyandre <friliyandre72004@gmail.com>
2026-09-20 00:42:32 +0700

    toast notification

--- frontend/src/services/api/authApi.ts
+++ frontend/src/services/api/authApi.ts
@@ -3,5 +3,5 @@ import type { ApiResponse, LoginRequest, LoginResponse } from "../../types";
 
 export const authApi = {
   login: (data: LoginRequest) =>
-    apiClient.post<ApiResponse<LoginResponse>>("/login", data),
+    apiClient.post<ApiResponse<LoginResponse>>("v1/login", data),
 };
