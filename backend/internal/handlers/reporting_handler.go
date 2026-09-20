39204eab22d55b21484f34a320fbdb16216d1100
bella saharani <intern.bella@gli.id>
2026-09-20 17:11:25 +0700

    feat: :sparkles: add new endpoint
    GET /reporting/auction-summary

--- backend/internal/handlers/reporting_handler.go
+++ backend/internal/handlers/reporting_handler.go
@@ -152,6 +152,7 @@ func (h *Handler) GetTotalTransaction(c *gin.Context) {
 		"data":    totalTransactions,
 	})
 }
+
 func (h *Handler) GetAuctionSummary(c *gin.Context) {
 	summary, err := h.reportingService.GetAuctionSummary(c.Request.Context())
 	if err != nil {
